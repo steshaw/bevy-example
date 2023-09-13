fb4aef315758f9f33869333a5e274417a5df51e6
Steven Shaw <steven@steshaw.org>
2023-09-13 13:05:04 +1000

    Panic: resource GreetTimer does not exist

--- src/main.rs
+++ src/main.rs
@@ -27,11 +27,16 @@ fn hello_world() {
     println!("hello world!");
 }
 
-fn greet_people(time: Res<Time>, query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello name={} time={:?}!", name.0, time.delta());
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello name={} time={:?}!", name.0, time.delta());
+        }
     }
 }
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
 
 pub struct OurPlugin;
 
