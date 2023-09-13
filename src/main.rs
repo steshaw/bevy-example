2d7cbd7bec2e60122ac3bae367d9aaaa0695a38d
Steven Shaw <steven@steshaw.org>
2023-09-13 12:35:58 +1000

    Simple system

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Position { x: f32, y: f32 }
+
+fn print_position_system(query: Query<&Position>) {
+    for position in &query {
+        println!("position: {} {}", position.x, position.y);
+    }
+}
+
+struct Entity(u64);
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn hello_world() {
+    println!("hello world!");
+}
+
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Update, hello_world)
+        .run();
 }
